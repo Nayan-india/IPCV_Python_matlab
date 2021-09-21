c05ccd3a544c47cc6acf64d85a3ccd04507eee8c
unknown <agarwalnayan25@gmail.com>
2021-09-21 11:06:39 +0530

    Added ipcv_005.py, ipcv_006.py, ipcv_006.py, ipcv_008.py

---
+++ ipcv_005.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+side = 400
+
+img = np.ones([side, side, 3], dtype = np.uint8)
+img[:] = 255
+
+for y in range(0, side):
+    for x in range(0, side): 
+        if x == y:
+            img[y, x] = 0
+    
+#cv.imshow("Diagonal",img)
+plt.imshow(img)
+plt.show()
---
+++ ipcv_007.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+side = 400
+
+img = np.ones([side, side, 3], dtype = np.uint8)
+img[:] = 255
+
+for y in range(0, side):
+    for x in range(0, side): 
+        if x < y:
+            img[y, x] = 0
+    
+plt.imshow(img)
+plt.show()
